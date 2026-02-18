239a67de42366725666687f2c1b38b85415f0fd5
karan-chaudhary-69 <karan00chy@gmail.com>
2026-02-19 05:16:53 +0545

    Uncomment server listen code

--- server.js
+++ server.js
@@ -162,6 +162,6 @@ app.post('/verify-otp', async (req, res) => {
 // Start Server
 // -----------------
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, "0.0.0.0", () => {
-//     console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
-// });
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+});
