15f79c1a2b561d3d78d97dc4365408d0ead2f63a
karan-chaudhary-69 <karan00chy@gmail.com>
2026-02-18 21:58:56 +0000

    change

--- models/Otp.js
+++ models/Otp.js
@@ -1,9 +1,8 @@
-const mongoose = require('mongoose');
-
-const otpSchema = new mongoose.Schema({
-    email: { type: String, required: true },
-    otp: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 300 } // Auto-delete after 5 mins (300 seconds)
+await mongoose.connect(process.env.MONGO_URI);   // MongoDB
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
-
-module.exports = mongoose.model('Otp', otpSchema);
