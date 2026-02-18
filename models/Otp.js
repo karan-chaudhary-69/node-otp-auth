ff53b11a16d1637009bbb59b9160b5f9a7af05c0
karan-chaudhary-69 <karan00chy@gmail.com>
2026-02-19 05:31:35 +0545

    Update Otp.js

--- models/Otp.js
+++ models/Otp.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const OtpSchema = new mongoose.Schema({
-  email: String,
-  otp: String,
-  createdAt: Date,
-  attempts: Number,
-  lockUntil: Date
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, index: { expires: 600 } }, // 10 min TTL
+  attempts: { type: Number, default: 0 },
+  lockUntil: { type: Date }
 });
 
 module.exports = mongoose.model('Otp', OtpSchema);
